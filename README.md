f0004742ac297452c6e45195e3d858a7b3f76e58
andrienko <andrienko@my.com>
2014-04-28 10:32:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Spin
+===
+Spin is the kind of a script to provide a new input type. It is supported only by modern browsers.
+
+Usage
+---
+
+All you gotta do is add spin.js to your page:
+
+    <script type="text/javascript" src="spin.js"></script>
+
+And then you create an input element with type = "spin"
+
+    <input type="spin" step="1"/>
+
+Now, if you scroll wheel up or down or drag the element up or down - the contents of this field will be increased or
+decreased by 1 respectively.
+
+Features
+---
+ - If step is not specified - it will be equal to 5
+ - Step can be float and/or negative number
+ - The type="spin" changes to type="text" after script is initialized. This may change in future
---
+++ spin.js
@@ -0,0 +1,50 @@
+var a_inputSpin = {
+    activeSpin:null,
+    lastState:null,
+    init:function(){
+        var spins = Array.prototype.slice.call(document.querySelectorAll('input[type=spin]'));
+        spins.forEach(function(el){
+            a_inputSpin.initElement(el);
+        });
+    },
+    initElement:function(element){
+        element.setAttribute('type','text');
+        element.addEventListener('mousedown',this.select);
+        element.addEventListener("mousewheel", this.wheel, false);
+        element.addEventListener("DOMMouseScroll", this.wheel, false);
+    },
+    select:function(event){
+        a_inputSpin.activeSpin = this;
+        a_inputSpin.lastState = event;
+    },
+    move:function(event){
+        if(!this.activeSpin)return;
+
+        this.changeValue(this.activeSpin,this.calculateDelta(event));
+    },
+    deselect:function(){
+        this.activeSpin=null;
+    },
+    wheel:function(e){
+        a_inputSpin.changeValue(this,Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail))));
+    },
+    changeValue:function(element,delta){
+        if(delta==0)return;
+        var step = parseFloat(element.getAttribute('step'));
+        var val = parseFloat(element.value);
+        element.value = isNaN(val)?0:val + (isNaN(step)?5:step)*delta;
+    },
+    calculateDelta:function(event){
+        if(a_inputSpin.lastState.y == event.y)return 0;
+        var delta = (a_inputSpin.lastState.y > event.y) * 2 - 1;
+        a_inputSpin.lastState = event;
+        return delta;
+    }
+};
+
+window.addEventListener('load',function(){
+    a_inputSpin.init();
+});
+
+window.addEventListener('mousemove',function(e){a_inputSpin.move(e);});
+window.addEventListener('mouseup',function(){a_inputSpin.deselect();});
